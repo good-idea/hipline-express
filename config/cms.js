d32dea21e833eeca2b2aa5f63936c85885173ce1
Joseph Thomas <info@standard-quality.biz>
2017-07-18 18:58:25 -0700

    CMS .env config update

--- config/cms.js
+++ config/cms.js
@@ -2,5 +2,5 @@ const environment = require('./environment');
 
 module.exports = {
 	host: environment.CMS_HOST || 'localhost',
-	port: environment.CMS_PORT || 8088,
+	port: environment.CMS_PORT || 80,
 };
