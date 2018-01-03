e83f9f5d44fe4c247ad8db4536222112171449df
Joseph Thomas <info@standard-quality.biz>
2018-01-03 12:36:43 -0800

    Remove redis caching

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,38 +1,21 @@
-const redis = require('redis')
+// const redis = require('redis')
 const axios = require('axios')
 const { promisify } = require('util')
 const { slugify } = require('../utils/text')
 const git = require('git-rev-sync')
 const { cms } = require('../config')
 
-const client = redis.createClient()
+// const client = redis.createClient()
 
-const getCache = promisify(client.get).bind(client)
-const setCache = promisify(client.set).bind(client)
+// const getCache = promisify(client.get).bind(client)
+// const setCache = promisify(client.set).bind(client)
 
 module.exports.site = function getPage(req, res, next) {
 	const uri = req.url
-	const slug = slugify(uri)
-	getCache(slug)
-		.then(cached => {
-			if (cached) {
-				const meta = JSON.parse(cached)
-				return res.render('index', { meta })
-			}
-			axios
-				.get(`${cms.apiRoot}/initial?uri=${uri}`)
-				.then(response => {
-					console.log(git.short())
-					setCache(
-						slug,
-						JSON.stringify(response.data.meta),
-						'EX',
-						60 * 15,
-					).then(() =>
-						res.render('index', { meta: response.data.meta, rev: git.short() }),
-					)
-				})
-				.catch(next)
+	axios
+		.get(`${cms.apiRoot}/initial?uri=${uri}`)
+		.then(response => {
+			res.render('index', { meta: response.data.meta, rev: git.short() })
 		})
 		.catch(next)
 }
