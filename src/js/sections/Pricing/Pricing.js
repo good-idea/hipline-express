91dfcfff9c92c2fdf51b23c3021675fc7f42fd14
Joseph Thomas <joseph@good-idea.studio>
2018-12-14 11:19:52 -0800

    make pricing column wider

--- src/js/sections/Pricing/Pricing.js
+++ src/js/sections/Pricing/Pricing.js
@@ -9,28 +9,26 @@ import Splash from '../Splash'
  * Pricing
  */
 
-const Pricing = props => {
-	return (
-		<ScrollableContainer containerElement={document.body}>
-			<div>
-				<Splash text={props.splashText} />
-				<section className="classes with-aside">
-					<main>
-						<div className="column--wide">
-							<div>
-								{props.passes.types.map(p => (
-									<ScrollableChild key={`trigger-passSection-${p.slug}`} slug={`passSection-${p.slug}`}>
-										<PassSection {...p} />
-									</ScrollableChild>
-								))}
-							</div>
+const Pricing = props => (
+	<ScrollableContainer containerElement={document.body}>
+		<div>
+			<Splash text={props.splashText} />
+			<section className="classes with-aside">
+				<main>
+					<div className="column--xWide">
+						<div>
+							{props.passes.types.map(p => (
+								<ScrollableChild key={`trigger-passSection-${p.slug}`} slug={`passSection-${p.slug}`}>
+									<PassSection {...p} />
+								</ScrollableChild>
+							))}
 						</div>
-					</main>
-				</section>
-			</div>
-		</ScrollableContainer>
-	)
-}
+					</div>
+				</main>
+			</section>
+		</div>
+	</ScrollableContainer>
+)
 
 Pricing.propTypes = {
 	passes: PropTypes.shape({
--- src/js/utils/content.js
+++ src/js/utils/content.js
@@ -191,6 +191,7 @@ export const groupClassTypes = content => {
 }
 
 const separateFooterPages = content => {
+	// console
 	const { footerPages, infoPages } = R.groupBy(p => (p.is_footer || p.nav_settings === 'footer' ? 'footerPages' : 'infoPages'), [
 		...content.infoPages,
 		content.choreographers,
