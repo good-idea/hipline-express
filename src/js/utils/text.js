b73e02464db6b22d1ede2b9dcecd9aa3680efdeb
Joseph Thomas <joseph@good-idea.studio>
2019-03-17 18:47:40 -0700

    fix: don't strip strong or em tags

--- src/js/utils/text.js
+++ src/js/utils/text.js
@@ -105,7 +105,7 @@ const makeParagraph = R.pipe(
 	fixKirbyText,
 	markdownToHTML,
 	externalLinks,
-	stripTags('<a><br><p><li><ul><ol><h1><h2><h3><h4>'),
+	stripTags('<a><br><p><li><ul><ol><h1><h2><h3><h4><em><strong>'),
 	HTMLtoJSX,
 )
 
