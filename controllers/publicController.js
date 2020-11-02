013ad0bf5c57a457272cc221c55d3be013496eef
Joseph Thomas <joseph@good-idea.studio>
2020-11-01 21:58:09 -0800

    sanitize html for snippets

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 // const { promisify } = require('util')
 // const { slugify } = require('../utils/text')
 const git = require('git-rev-sync')
+const sanitizeHtml = require('sanitize-html')
 const { cms } = require('../config')
 
 // const client = redis.createClient()
@@ -10,12 +11,21 @@ const { cms } = require('../config')
 // const getCache = promisify(client.get).bind(client)
 // const setCache = promisify(client.set).bind(client)
 
+// const lenses = [
+//   R.lens(R.path(''))
+//
+// ]
+
 module.exports.site = function getPage(req, res, next) {
   const uri = req.url
   axios
     .get(`${cms.apiRoot}/initial?uri=${uri}`)
     .then((response) => {
-      return res.render('index', { site: response.data.site, meta: response.data.meta || {}, rev: git.short() })
+      const site = Object.assign(response.data.site, {
+        headsnippets: sanitizeHtml(response.data.site.headsnippets || ''),
+        bodysnippets: sanitizeHtml(response.data.site.bodysnippets || ''),
+      })
+      return res.render('index', { site, meta: response.data.meta || {}, rev: git.short() })
     })
     .catch(next)
 }
--- controllers/sitemap.js
+++ controllers/sitemap.js
@@ -3,42 +3,41 @@ const axios = require('axios')
 const { cms } = require('../config')
 
 const buildSitemap = (req, res) => {
-	axios.get(`${cms.apiRoot}/initial`).then(response => {
-		const { home, classes, infoPages, sourcePasses } = response.data
-		console.log(Object.keys(response.data))
-		const homepageUrl = { url: '/', changeFreq: 'weekly', lastmodISO: home.lastModified, priority: 0.8 }
-		const classesUrl = { url: '/classes', changeFreq: 'weekly', lastModISO: classes.lastModified, priority: 0.8 }
-		const pricingUrl = { url: '/passes', changeFreq: 'weekly', lastModISO: sourcePasses.lastModified, priority: 0.8 }
-		const infoPageUrls = infoPages
-			.filter(page => page.isVisible)
-			.map(page => ({
-				url: `/${page.slug}`,
-				changeFreq: 'weekly',
-				lastModISO: page.lastModified,
-				priority: 0.7,
-			}))
+  axios.get(`${cms.apiRoot}/initial`).then((response) => {
+    const { home, classes, infoPages, sourcePasses } = response.data
+    const homepageUrl = { url: '/', changeFreq: 'weekly', lastmodISO: home.lastModified, priority: 0.8 }
+    const classesUrl = { url: '/classes', changeFreq: 'weekly', lastModISO: classes.lastModified, priority: 0.8 }
+    const pricingUrl = { url: '/passes', changeFreq: 'weekly', lastModISO: sourcePasses.lastModified, priority: 0.8 }
+    const infoPageUrls = infoPages
+      .filter((page) => page.isVisible)
+      .map((page) => ({
+        url: `/${page.slug}`,
+        changeFreq: 'weekly',
+        lastModISO: page.lastModified,
+        priority: 0.7,
+      }))
 
-		const sitemap = sm.createSitemap({
-			hostname: 'https://www.myhipline.com',
-			cacheTime: 600000, // 600 sec - cache purge period
-			urls: [
-				//
-				homepageUrl,
-				classesUrl,
-				pricingUrl,
-				...infoPageUrls,
-			].filter(Boolean),
-		})
+    const sitemap = sm.createSitemap({
+      hostname: 'https://www.myhipline.com',
+      cacheTime: 600000, // 600 sec - cache purge period
+      urls: [
+        //
+        homepageUrl,
+        classesUrl,
+        pricingUrl,
+        ...infoPageUrls,
+      ].filter(Boolean),
+    })
 
-		return sitemap.toXML((err, xml) => {
-			if (err) {
-				console.log(err)
-				return res.status(500).end()
-			}
-			res.header('Content-Type', 'application/xml')
-			res.send(xml)
-		})
-	})
+    return sitemap.toXML((err, xml) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).end()
+      }
+      res.header('Content-Type', 'application/xml')
+      res.send(xml)
+    })
+  })
 }
 
 module.exports = buildSitemap
