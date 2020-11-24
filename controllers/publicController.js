2cbed7c66efa3c54a732dcc57cde20920804fe37
Joseph Thomas <joseph@good-idea.studio>
2020-11-24 10:31:24 -0800

    Revert "sanitize html for snippets"
    This reverts commit 013ad0bf5c57a457272cc221c55d3be013496eef.

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 // const { promisify } = require('util')
 // const { slugify } = require('../utils/text')
 const git = require('git-rev-sync')
-const sanitizeHtml = require('sanitize-html')
 const { cms } = require('../config')
 
 // const client = redis.createClient()
@@ -11,21 +10,12 @@ const { cms } = require('../config')
 // const getCache = promisify(client.get).bind(client)
 // const setCache = promisify(client.set).bind(client)
 
-// const lenses = [
-//   R.lens(R.path(''))
-//
-// ]
-
 module.exports.site = function getPage(req, res, next) {
   const uri = req.url
   axios
     .get(`${cms.apiRoot}/initial?uri=${uri}`)
     .then((response) => {
-      const site = Object.assign(response.data.site, {
-        headsnippets: sanitizeHtml(response.data.site.headsnippets || ''),
-        bodysnippets: sanitizeHtml(response.data.site.bodysnippets || ''),
-      })
-      return res.render('index', { site, meta: response.data.meta || {}, rev: git.short() })
+      return res.render('index', { site: response.data.site, meta: response.data.meta || {}, rev: git.short() })
     })
     .catch(next)
 }
