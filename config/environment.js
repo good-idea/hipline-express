af7351dafe0142348284e0ce7df3845ab1136b34
Joseph Thomas <joseph@good-idea.studio>
2021-02-25 13:49:29 -0800

    ignore build files

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,14 @@
 const dotenv = require('dotenv').config()
 
-module.exports = dotenv.parsed || {}
+const config = {
+  // the host & port to connect to the CMS
+  CMS_HOST: process.env.CMS_HOST || 'panel.myhipline.com',
+  CMS_PORT: process.env.CMS_PORT || 80,
+  // The port that express runs on
+  PORT: process.env.PORT || 3000,
+  ENV: process.env.ENV || 'production',
+  // Allow a .env file to override any of the above
+  ...dotenv.parsed,
+}
+
+module.exports = config
